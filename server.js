03b1be6c4615b5e8cb7846bb9ea050f10b786f22
Stian Westvig <stian.westvig@bouvet.no>
2016-11-04 15:01:35 +0100

    added api endpoint

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 
+const views = require('./routes/views');
+const api = require('./routes/api');
+
 const app = express();
 
 app.engine('handlebars', handlebars({
   defaultLayout: 'main',
-  layoutsDir: __dirname + '/src/views/layouts'
+  layoutsDir: __dirname + '/views/layouts'
 }));
-app.set('views', __dirname + '/src/views');
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.use('/api', api);
+app.use('/', views);
 
 app.listen(3000, () => {
   console.log('App is now running on port 3000');
